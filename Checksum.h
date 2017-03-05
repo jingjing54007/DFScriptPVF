7ca9987ba97bd610993a659495d50219c31d3124
Kao Fan <gaofan2015@gmail.com>
2017-03-05 12:51:32 +0800

    Del

--- Checksum.h
+++
@@ -1,4 +0,0 @@
-void Generatesbox(unsigned int* sbox);
-unsigned int CalculateChecksum(unsigned char* block, int lengthOfBlock, unsigned int curChecksum);
-unsigned int CalculateChecksumFile(char* fileName);
-void PVFChecksum(unsigned char* block, int lengthOfBlock, unsigned int numFiles, unsigned int checksum);
--- ModesOp.h
+++
@@ -1,5 +0,0 @@
-void Create(void);
-void Unpack(void);
-void Diff(void);
-void Decrypt(void);
-void Debug(void);
